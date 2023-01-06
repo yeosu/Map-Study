190e0ed4214eac33641d4f3ec69c56d23fb8c362
eunhyeok <ceh2805@gmail.com>
2023-01-06 22:23:40 +0900

    #카카오 api 연동준비
    1) 연동에 필요한 dto, service, config 생성
    2) application.yml에 카카오 키 추가

---
+++ src/main/java/com/example/map/api/dto/KakaoApiResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.map.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class KakaoApiResponseDto {
+
+    private MetaDto metaDto;
+
+    private List<DocumentDto> documentList;
+}
