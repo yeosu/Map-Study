0e38a6662d5eb366fc0e09bbc65df5478cc65492
eunhyeok <ceh2805@gmail.com>
2023-01-09 21:41:46 +0900

    #테스트 환경 세팅
    1) spock 플러그인 추가
    2) kskaoUriBuilder service 생성

--- src/main/java/com/example/map/api/dto/KakaoApiResponseDto.java
+++ src/main/java/com/example/map/api/dto/KakaoApiResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.map.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,9 @@ import java.util.List;
 @NoArgsConstructor
 public class KakaoApiResponseDto {
 
+    @JsonProperty("meta")
     private MetaDto metaDto;
 
+    @JsonProperty("documents")
     private List<DocumentDto> documentList;
 }
